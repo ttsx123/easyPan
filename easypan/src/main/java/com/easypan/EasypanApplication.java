793306ec143d16ad202866eb6a12d15ef30c3d24
ttsx123 <108857716+ttsx123@users.noreply.github.com>
2023-06-13 13:29:34 +0800

    开始

---
+++ easypan/src/main/java/com/easypan/EasypanApplication.java
@@ -0,0 +1,13 @@
+package com.easypan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasypanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasypanApplication.class, args);
+    }
+
+}
---
+++ easypan/src/test/java/com/easypan/EasypanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easypan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasypanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
